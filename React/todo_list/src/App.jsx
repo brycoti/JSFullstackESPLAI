2ac8727bcbe50c26aa5f58f41c0c2ba2b034266d
brycoti <bryancozartitos@gmail.com>
2024-02-12 18:47:43 +0100

    react proyectos con Link y rutas

---
+++ React/todo_list/src/App.jsx
@@ -0,0 +1,85 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [tareas, setTareas] = useState([]);
+  const [tarea, setTarea] = useState('');
+  const [color, setColor] = useState('TRABAJO'); // Valor inicial pq no puede estar vacío
+
+  const colorClases = {
+    TRABAJO: 'text-blue-500',
+    PERSONAL: 'text-green-500',
+    URGENTE: 'text-red-500',
+    FAMILIA: 'text-orange-500',
+  };
+
+  const handleSubmit = (event) => {
+
+    event.preventDefault();
+
+    if (tarea !== '') {
+      const nuevaTarea = { texto: tarea, color: color };
+      setTareas([...tareas, nuevaTarea]); // agregar el objeto nuevaTarea 
+      setTarea(''); // Limpiar el campo de entrada después de agregar la tarea
+    }
+  };
+
+  const cambiaColor = (event) => { 
+    setColor(event.target.value);
+  };
+
+  const nuevaTarea = (event) => {
+    setTarea(event.target.value);
+  };
+
+  const eliminaTarea = (id) => { 
+    const nuevasTareas = tareas.filter((_, index) => index !== id); 
+    setTareas(nuevasTareas);
+  };
+
+  //Renderiza el color con una clase Tailwind con el color del formulario
+  const listaTareas = tareas.map((tarea, indice) => (
+    <div key={indice} className={`flex justify-between items-center ${colorClases[tarea.color]} mb-2 py-2 px-4 rounded`}>
+      
+      <button className='bg-orange-200 hover:bg-orange-400  py-2 px-4 rounded' onClick={() => eliminaTarea(indice)}>
+        <span>{tarea.texto}</span>
+      </button>
+    </div>
+  ));
+
+  return (
+    <>
+      <div className="flex flex-row place-items-start ">
+        <form onSubmit={handleSubmit} className="m-4">
+          <div>
+            <h2 className="mb-3 text-2xl text-red-800">Introduce la nueva tarea</h2><br />
+            <input value={tarea} onChange={nuevaTarea} name="nom" type="text" id="nom" placeholder='Nom'
+            className="input bg-white appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500" />
+          </div>
+
+          <div>
+            <select onChange={cambiaColor} value={color} className="mt-4 bg-white border-2 border-gray-200 rounded py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500">
+              <option value="TRABAJO">Trabajo (azul)</option>
+              <option value="PERSONAL">Personal (verde)</option>
+              <option value="URGENTE">Urgente (rojo)</option>
+              <option value="FAMILIA">Familia (naranja)</option>
+            </select>
+          </div>
+
+          <button className="bg-red-400 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mt-4" type="submit">Enviar</button>
+        </form>
+
+        <div className="w-full px-4  flex flex-col items-center border-l-2 border-red-400">
+          <h2 className="text-3xl mb-4 text-red-800">Listado de Tareas</h2>
+            <div className='grid  grid grid-cols-2 grid-rows-auto '>
+              {listaTareas}
+            </div>
+            
+           
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
